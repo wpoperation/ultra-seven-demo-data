74a4d0c6d2e6f3c1ce4fdee0b37cb5a189f8ac0a
sagar <girisagar051@gmail.com>
2019-07-02 22:08:56 +0545

    added config.php

---
+++ config.php
@@ -0,0 +1,134 @@
+<?php
+
+      $data = array(
+        'lifestyle' => array(
+          'categories'        => array( 'Lifestyle' ),
+          'preview_url' => 'https://demo.wpoperation.com/ultraseven/lifestyle/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '5',
+          'is_shop' => false,
+          'menus'         => array(
+              'main-menu' => 'Main Menu',
+              'top-menu' => 'Top Menu',
+              'footer-menu' => 'Footer Menu'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+              array(
+                'slug'    => 'woocommerce',
+                'init'    => 'woocommerce/woocommerce.php',
+                'name'    => 'WooCommerce',
+              ),
+              array(
+                'slug'    => 'mailpoet',
+                'init'    => 'mailpoet/mailpoet.php',
+                'name'    => 'Mailpoet',
+              ),
+            ),
+          ),
+        ),
+        'fashion' => array(
+          'categories'        => array( 'Fashion' ),
+          'preview_url' => 'https://demo.wpoperation.com/ultraseven/fashion/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '5',
+          'is_shop' => false,
+          'menus'         => array(
+              'main-menu' => 'Main Menu',
+              'top-menu' => 'Top Menu',
+              'footer-menu' => 'Footer Menu'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+              array(
+                'slug'    => 'woocommerce',
+                'init'    => 'woocommerce/woocommerce.php',
+                'name'    => 'WooCommerce',
+              ),
+              array(
+                'slug'    => 'yith-woocommerce-wishlist',
+                'init'    => 'yith-woocommerce-wishlist/yith-woocommerce-wishlist.php',
+                'name'    => 'YITH WooCommerce Wishlist',
+              ),
+            ),
+          ),
+        ),
+        'sports' => array(
+          'categories'        => array( 'Sports' ),
+          'preview_url' => 'https://demo.wpoperation.com/ultraseven/sports/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '5',
+          'is_shop' => false,
+          'menus'         => array(
+              'main-menu' => 'Main Menu',
+              'top-menu' => 'Top Menu',
+              'footer-menu' => 'Footer Menu'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              )
+            ),
+          ),
+        ),
+        'blog' => array(
+          'categories'        => array( 'Blog' ),
+          'preview_url' => 'https://demo.wpoperation.com/ultraseven/blog/',
+          'xml_file'        => 'content.xml',
+          'theme_settings'  => 'options.dat',
+          'widgets_file'    => 'widgets.wie',
+          'home_title'      => 'Home',
+          'blog_title'      => 'Blog',
+          'posts_to_show'   => '5',
+          'is_shop' => false,
+          'menus'         => array(
+              'main-menu' => 'Main Menu',
+              'top-menu' => 'Top Menu',
+              'footer-menu' => 'Footer Menu'
+          ),
+          'required_plugins'  => array(
+            'free' => array(
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+              array(
+                'slug'    => 'instashow-lite',
+                'init'    => 'instashow-lite/instashow-lite.php',
+                'name'    => 'Instashow Lite',
+              ),
+              array(
+                'slug'    => 'smart-slider-3',
+                'init'    => 'smart-slider-3/smart-slider-3.php',
+                'name'    => 'Smart Slider 3',
+              ),
+            ),
+          ),
+        ),
+      );
